39e8cc8d14ba69540f5a2bde5cb3a0b64e868fd9
Feng Peiyuan <peiyuan.feng@peiyuanfeng-mac.local>
2020-02-28 15:21:19 +0800

    redis set flush data

--- go.mod
+++ go.mod
@@ -1,6 +1,9 @@
 module github.com/rakyll/hey
 
 require (
+	github.com/go-redis/redis v6.15.7+incompatible
 	golang.org/x/net v0.0.0-20181017193950-04a2e542c03f
 	golang.org/x/text v0.3.0 // indirect
 )
+
+go 1.13
--- requester/requester.go
+++ requester/requester.go
@@ -1,299 +1,130 @@
-// Copyright 2014 Google Inc. All Rights Reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-// Package requester provides commands to run load tests and display results.
 package requester
 
 import (
-  //"fmt"
-  "log"
-  "strconv"
 	"bytes"
-	"crypto/tls"
-	"io"
-	"io/ioutil"
-	"net/http"
-	"net/http/httptrace"
-	"net/url"
-	"os"
+	"errors"
+	"fmt"
+	"github.com/go-redis/redis"
+	"log"
+
 	"sync"
 	"time"
-  	"math/rand"
-	"golang.org/x/net/http2"
 )
 
 // Max size of the buffer of result channel.
 const maxResult = 1000000
 const maxIdleConn = 500
 
-type result struct {
-	err           error
-	statusCode    int
-	offset        time.Duration
-	duration      time.Duration
-	connDuration  time.Duration // connection setup(DNS lookup + Dial up) duration
-	dnsDuration   time.Duration // dns lookup duration
-	reqDuration   time.Duration // request "write" duration
-	resDuration   time.Duration // response "read" duration
-	delayDuration time.Duration // delay between response and request
-	contentLength int64
-}
-
 type Work struct {
-	// Request is the request to be made.
-	Request *http.Request
-
-	RequestBody []byte
-
-	// N is the total number of requests to make.
-	N int
-
 	// C is the concurrency level, the number of concurrent workers to run.
 	C int
 
-	// H2 is an option to make HTTP/2 requests
-	H2 bool
-
-	// Timeout in seconds.
-	Timeout int
-
-	// Qps is the rate limit in queries per second.
-	QPS float64
-
-	// DisableCompression is an option to disable compression in response
-	DisableCompression bool
-
-	// DisableKeepAlives is an option to prevents re-use of TCP connections between different HTTP requests
-	DisableKeepAlives bool
-
-	// DisableRedirects is an option to prevent the following of HTTP redirects
-	DisableRedirects bool
-
-	// Output represents the output type. If "csv" is provided, the
-	// output will be dumped as a csv stream.
-	Output string
-
-	// ProxyAddr is the address of HTTP proxy server in the format on "host:port".
-	// Optional.
-	ProxyAddr *url.URL
-
-	// Writer is where results will be written. If nil, results are written to stdout.
-	Writer io.Writer
-
 	initOnce sync.Once
-	results  chan *result
 	stopCh   chan struct{}
-	start    time.Duration
 
-	report *report
-
-  	IsDynamicUrl bool
-  	DynamicUrlFrom  int64
-  	DynamicUrlTo   int64
-  	DynamicUrlPrefix string
+	DynamicFrom   int64
+	DynamicTo     int64
+	DynamicPrefix string
+	RedisAddress  string
+	CommandFormat string
+	BodySize      int64
+	bodyBuffer    bytes.Buffer
 }
 
-func (b *Work) writer() io.Writer {
-	if b.Writer == nil {
-		return os.Stdout
-	}
-	return b.Writer
-}
-
-// Init initializes internal data-structures
 func (b *Work) Init() {
 	b.initOnce.Do(func() {
-		b.results = make(chan *result, min(b.C*1000, maxResult))
 		b.stopCh = make(chan struct{}, b.C)
+		for i := 0; int64(i) < b.BodySize; i++ {
+			b.bodyBuffer.WriteString("z")
+		}
 	})
+	log.Printf("[Init] body: %s", b.bodyBuffer.String())
 }
 
-// Run makes all the requests, prints the summary. It blocks until
-// all work is done.
 func (b *Work) Run() {
 	b.Init()
-	b.start = now()
-	b.report = newReport(b.writer(), b.results, b.Output, b.N)
-	// Run the reporter first, it polls the result channel until it is closed.
-	go func() {
-		runReporter(b.report)
-	}()
 	b.runWorkers()
 	b.Finish()
 }
 
 func (b *Work) Stop() {
-	// Send stop signal so that workers can stop gracefully.
 	for i := 0; i < b.C; i++ {
 		b.stopCh <- struct{}{}
 	}
 }
 
 func (b *Work) Finish() {
-	close(b.results)
-	total := now() - b.start
-	// Wait until the reporter is done.
-	<-b.report.done
-	b.report.finalize(total)
+
 }
 
-func (b *Work) makeRequest(c *http.Client) {
-	s := now()
-	var size int64
-	var code int
-	var dnsStart, connStart, resStart, reqStart, delayStart time.Duration
-	var dnsDuration, connDuration, resDuration, reqDuration, delayDuration time.Duration
-	req := cloneRequest(b.Request, b.RequestBody,b.IsDynamicUrl,b.DynamicUrlFrom,b.DynamicUrlTo,b.DynamicUrlPrefix)
-	trace := &httptrace.ClientTrace{
-		DNSStart: func(info httptrace.DNSStartInfo) {
-			dnsStart = now()
-		},
-		DNSDone: func(dnsInfo httptrace.DNSDoneInfo) {
-			dnsDuration = now() - dnsStart
-		},
-		GetConn: func(h string) {
-			connStart = now()
-		},
-		GotConn: func(connInfo httptrace.GotConnInfo) {
-			if !connInfo.Reused {
-				connDuration = now() - connStart
-			}
-			reqStart = now()
-		},
-		WroteRequest: func(w httptrace.WroteRequestInfo) {
-			reqDuration = now() - reqStart
-			delayStart = now()
-		},
-		GotFirstResponseByte: func() {
-			delayDuration = now() - delayStart
-			resStart = now()
-		},
+func (b *Work) makeRequest(cli *redis.Client, n int64) {
+	key := fmt.Sprintf("key:%012d", n)
+	//log.Printf("[makeRequest] n: %012d, key:%s", n, key)
+	v, er := cli.Do("SET", key, b.bodyBuffer.String()).Result()
+	if er != nil {
+		log.Printf("[makeRequest] Do fail, err: %v, return:%s", er, v)
 	}
-	req = req.WithContext(httptrace.WithClientTrace(req.Context(), trace))
-	resp, err := c.Do(req)
-	if err == nil {
-		size = resp.ContentLength
-		code = resp.StatusCode
-		io.Copy(ioutil.Discard, resp.Body)
-		resp.Body.Close()
+	if n%10000 == 0 {
+		log.Printf("[-- makeRequest --] request: %d completed.", n)
 	}
-	t := now()
-	resDuration = t - resStart
-	finish := t - s
-	b.results <- &result{
-		offset:        s,
-		statusCode:    code,
-		duration:      finish,
-		err:           err,
-		contentLength: size,
-		connDuration:  connDuration,
-		dnsDuration:   dnsDuration,
-		reqDuration:   reqDuration,
-		resDuration:   resDuration,
-		delayDuration: delayDuration,
-	}
+
 }
 
-func (b *Work) runWorker(client *http.Client, n int) {
-	var throttle <-chan time.Time
-	if b.QPS > 0 {
-		throttle = time.Tick(time.Duration(1e6/(b.QPS)) * time.Microsecond)
+func (b *Work) runWorker(start, end int64) {
+	//log.Printf("[runWorker] from:%d, to:%d", start, end)
+	cli, err := connectRedis(&redis.Options{Addr: b.RedisAddress, ReadTimeout: -1, WriteTimeout: -1})
+	if err != nil {
+		log.Printf("[runWorker] error %v", err)
 	}
+	defer cli.Close()
 
-	if b.DisableRedirects {
-		client.CheckRedirect = func(req *http.Request, via []*http.Request) error {
-			return http.ErrUseLastResponse
-		}
-	}
-	for i := 0; i < n; i++ {
-		// Check if application is stopped. Do not send into a closed channel.
+	for i := start; i < end; i++ {
 		select {
 		case <-b.stopCh:
 			return
 		default:
-			if b.QPS > 0 {
-				<-throttle
-			}
-			b.makeRequest(client)
+			b.makeRequest(cli, i)
 		}
 	}
+	log.Printf("[runWorker] completed request from:%d, to:%d", start, end)
 }
 
 func (b *Work) runWorkers() {
 	var wg sync.WaitGroup
 	wg.Add(b.C)
-
-	tr := &http.Transport{
-		TLSClientConfig: &tls.Config{
-			InsecureSkipVerify: true,
-			ServerName:         b.Request.Host,
-		},
-		MaxIdleConnsPerHost: min(b.C, maxIdleConn),
-		DisableCompression:  b.DisableCompression,
-		DisableKeepAlives:   b.DisableKeepAlives,
-		Proxy:               http.ProxyURL(b.ProxyAddr),
-	}
-	if b.H2 {
-		http2.ConfigureTransport(tr)
-	} else {
-		tr.TLSNextProto = make(map[string]func(string, *tls.Conn) http.RoundTripper)
-	}
-	client := &http.Client{Transport: tr, Timeout: time.Duration(b.Timeout) * time.Second}
-
-	// Ignore the case where b.N % b.C != 0.
+	sum := b.DynamicTo - b.DynamicFrom
+	step := sum / int64(b.C)
+	stepSum := b.DynamicFrom
+	log.Printf("[runWorkers] sum: %d, concurrent:%d, step:%d,stepSum:%d", sum, b.C, step, stepSum)
 	for i := 0; i < b.C; i++ {
-		go func() {
-			b.runWorker(client, b.N/b.C)
+		go func(start, end int64) {
+			b.runWorker(start, end)
 			wg.Done()
-		}()
+		}(stepSum, stepSum+step-1)
+		stepSum += step
 	}
 	wg.Wait()
 }
 
-// cloneRequest returns a clone of the provided *http.Request.
-// The clone is a shallow copy of the struct and its Header map.
-func cloneRequest(r *http.Request, body []byte,isDynamicUrl bool,dynamicUrlFrom int64,dynamicUrlTo int64,dynamicUrlPrefix string) *http.Request {
-	// shallow copy of the struct
-	r2 := new(http.Request)
-	*r2 = *r
-	//is dynamic url 
-	if isDynamicUrl {
-	  baseUrl := r2.URL.String()
-	  rand.Seed(time.Now().UnixNano())
-	  num := dynamicUrlFrom + rand.Int63n(dynamicUrlTo-dynamicUrlFrom)
-	  newUrl := baseUrl + dynamicUrlPrefix + strconv.FormatInt(num,10)
-	  //fmt.Println(newUrl)
-	  u, err := url.Parse(newUrl)
-	  if err != nil {
-	      log.Fatal(err)
-	  }
-	  r2.URL = u
+func connectRedis(opts *redis.Options) (*redis.Client, error) {
+	var cli *redis.Client
+	if opts.Addr == "" {
+		return nil, errors.New("address empty")
 	}
-	// deep copy of the Header
-	r2.Header = make(http.Header, len(r.Header))
-	for k, s := range r.Header {
-		r2.Header[k] = append([]string(nil), s...)
-	}
-	if len(body) > 0 {
-		r2.Body = ioutil.NopCloser(bytes.NewReader(body))
-	}
-	return r2
+	return cli, withRetry(3, func() error {
+		cli = redis.NewClient(opts)
+		return cli.Ping().Err()
+	})
 }
 
-func min(a, b int) int {
-	if a < b {
-		return a
+func withRetry(maxTries int, f func() error) error {
+	var err error
+	sleepTime := 1 * time.Second
+	for i := 1; i <= maxTries; i++ {
+		if err = f(); err == nil {
+			return nil
+		}
+		time.Sleep(sleepTime)
 	}
-	return b
+	return err
 }
